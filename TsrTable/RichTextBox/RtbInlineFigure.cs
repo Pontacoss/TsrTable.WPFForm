2c997fe27c168370908f1925af035dc36a10bd80
Pontacoss <pontacoss@gmail.com>
2024-04-11 21:43:37 +0900

    2024/4/11 InlineFigure 修正

---
+++ TsrTable/RichTextBox/RtbInlineFigure.cs
@@ -0,0 +1,53 @@
+﻿using C1.WPF.RichTextBox.Documents;
+using System.IO;
+using System.Windows.Media.Imaging;
+using TsrTable.TableData;
+
+namespace TsrTable.RichTextBox
+{
+    internal class RtbInlineFigure : C1InlineUIContainer, IRtbElement
+    {
+        public byte[] Binary { get; }
+        public C1Length ImageHeight { get; }
+        public C1Length ImageWidth { get; }
+
+        public RtbInlineFigure() { }
+
+        public RtbInlineFigure(byte[] binary, double height, double width)
+        {
+            Binary = binary;
+            Height = new C1Length(height);
+            Width = new C1Length(width);
+
+            using (var ms = new MemoryStream(Binary))
+            {
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = ms;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                var img = new System.Windows.Controls.Image();
+                img.BeginInit();
+                img.Source = bitmapImage;
+                img.EndInit();
+
+                Content = img;
+
+            }
+        }
+
+        //public override C1TextElement Clone()
+        //{
+        //    var content = Content as System.Windows.Controls.Image;
+        //    return new RtbInlineFigure(Binary, content.ActualHeight, content.ActualWidth);
+        //}
+
+        public ITsrElement GetTsrInstance()
+        {
+            var content = Content as System.Windows.Controls.Image;
+            return new TsrInlineFigure(Binary, content.ActualHeight, content.ActualWidth);
+        }
+    }
+}
--- TsrTable/TableData/TsrInlineFigure.cs
+++ TsrTable/TableData/TsrInlineFigure.cs
@@ -3,50 +3,51 @@ using C1.WPF.FlexGrid;
 using C1.WPF.RichTextBox.Documents;
 using C1.WPF.Word.Objects;
 using System;
-using System.Drawing;
-using System.IO;
 using System.Text.Json.Serialization;
 using System.Windows.Media.Imaging;
+using TsrTable.RichTextBox;
 
 namespace TsrTable.TableData
 {
     public sealed class TsrInlineFigure : ITsrElement
     {
         public byte[] Binary { get; } = null;
-        public C1Length ImageHeight { get; }
-        public C1Length ImageWidth { get; }
+
+        public double Height { get; }
+        public double Width { get; }
 
         [JsonConstructor]
-        public TsrInlineFigure(byte[] binary, C1Length imageHeight, C1Length imageWidth)
+        public TsrInlineFigure(byte[] binary, double height, double width)
         {
             Binary = binary;
-            ImageHeight = imageHeight;
-            ImageWidth = imageWidth;
+            Height = height;
+            Width = width;
         }
 
         public TsrInlineFigure(C1InlineUIContainer container)
         {
-            if (container.Content is BitmapImage bmp)
-            {
-                var stream = bmp.StreamSource;
-                Binary = new byte[stream.Length];
-                stream.Position = 0;
-                stream.Read(Binary, 0, (int)stream.Length);
-            }
-            else if (container.Content is System.Windows.Controls.Image img)
-            {
-                //todo 画像のデータ取り込みが出来ない。
-                BitmapImage source = img.Source as BitmapImage;
-                using (var fs = source.StreamSource)
-                {
-                    Binary = new byte[fs.Length];
-                    fs.Write(Binary, 0, (int)fs.Length);
-                    var writer = new BinaryWriter(fs);
-                    writer.Write(Binary);
-                }
-            }
-            ImageHeight = container.Height;
-            ImageWidth = container.Width;
+            //if (container.Content is BitmapImage bmp)
+            //{
+            var bmp = container.Content as BitmapImage;
+            var stream = bmp.StreamSource;
+            Binary = new byte[stream.Length];
+            stream.Position = 0;
+            stream.Read(Binary, 0, (int)stream.Length);
+            //}
+            //else if (container.Content is System.Windows.Controls.Image img)
+            //{
+            //    //todo 画像のデータ取り込みが出来ない。
+            //    BitmapImage source = img.Source as BitmapImage;
+            //    using (var fs = source.StreamSource)
+            //    {
+            //        Binary = new byte[fs.Length];
+            //        fs.Write(Binary, 0, (int)fs.Length);
+            //        var writer = new BinaryWriter(fs);
+            //        writer.Write(Binary);
+            //    }
+            //}
+            Height = container.Height.Value;
+            Width = container.Width.Value;
         }
 
         public RtfObject ToWord()
@@ -66,22 +67,7 @@ namespace TsrTable.TableData
 
         public C1TextElement GetRtbInstance()
         {
-            // バイナリデータからBitmp経由でBitmapImageに変換
-            using (var ms = new MemoryStream(Binary))
-            {
-                var bmp = new Bitmap(ms);
-                var bmpImage = bmp.ToBitmapImage();
-
-                return new C1InlineUIContainer()
-                {
-                    Content = new System.Windows.Controls.Image()
-                    {
-                        Source = bmpImage,
-                    },
-                    Height = ImageHeight,
-                    Width = ImageWidth,
-                };
-            }
+            return new RtbInlineFigure(Binary, Height, Width);
         }
 
 
